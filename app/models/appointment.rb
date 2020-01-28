4106a1cd53c2fff0f2f6b3078c4f933321658d7f
Brandon Weaver <a7xncob@gmail.com>
2020-01-28 23:13:56 +0000

    Automatically backed up by Learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,7 +3,7 @@ class Appointment < ApplicationRecord
   belongs_to :patient
   
   def appointment_datetime
-    return self.appointment_datetime.strftime("%B %d, %Y")
+    return self.appointment_datetime ? self.appointment_datetime.strftime("%B %d, %Y") : nil
   end
   
 end
